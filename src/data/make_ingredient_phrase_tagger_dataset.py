91c08a2680ca20bc3ccc1577489dfceec866d3aa
Josh Deragon <jtderagon@gmail.com>
2021-02-06 17:56:26 -0500

    Add quicker ingredient identification, different id methodology, quicker data cleaner, and updated parenthesis handling

--- src/data/make_ingredient_phrase_tagger_dataset.py
+++ src/data/make_ingredient_phrase_tagger_dataset.py
@@ -1,4 +1,6 @@
+import hashlib
 import pandas as pd
+from functools import reduce
 
 in_file_path = "../data/raw/train_file.csv"
 out_file_path = "../data/raw/ingredient_phrase_tagger_training_data.csv"
@@ -15,61 +17,74 @@ def column_names():
 			 'label'
 			]
 
-def add_id_to_dataset(df):
-	print("Adding an ID to ingredients...")
-	df['ID'] = None
-	ID_ = 0
-	df_len = len(df)
-	for i, row in df.iterrows():
-	    print(f"{round((i/df_len)*100,3)}%", end='\r')
-	    if row['text_index'] == 'I1':
-	        ID_ = str(int(ID_)+1).zfill(9)
-	    df.loc[i, "ID"] = ID_
-	return df
+def generate_id(s):
+    return hashlib.sha1(str(s).encode("utf-8")).hexdigest()
 
-def remove_label_prefix(label):
+def add_id_to_dataset(input_df):
+    print("Adding ID to dataset")
+    df = input_df.copy()
+    df['ID'] = None
+    new_ingredient_loc = df.text_index.str.match("^I1$")
+    ingredient_count = new_ingredient_loc.sum()
+    df.loc[new_ingredient_loc, "ID"] = list(map(generate_id, range(1,ingredient_count+1)))
+    return df.fillna(method = 'ffill')
+
+def remove_label_prefix(df):
     """Remove the prefix in labels such as 'B-'
     and 'I-'"""
-    if "-" in label: # if statement faster than split()
-        return label[2:]
-    return label
+    df.loc[:, 'label'] = df.label.replace('^(.-)', '', regex= True)
+    return df
 
-def replace_INDEX_with_QTY(label):
+def replace_index_with_qty(df):
     """Remove and replace 'INDEX' label with 'QTY'"""
-    if "INDEX" in label: # if statement faster than str.replace
-        return "QTY"
-    return label
+    df.loc[:, 'label'] = df.label.replace('INDEX', 'QTY')
+    return df
 
-def remove_QTY_symbol(text):
+def remove_qty_symbol(df):
     """Remove the '$' symbol from ingredient quantities"""
-    if "$" in text: # if faster when there is not a "$", slower when there is
-        return text.replace("$", ' ') 
-    return text
+    df.loc[:, 'text'] = df.text.replace('\$', ' ', regex = True)
+    return df
 
-def remove_parenthesis(input_df):
-    """Remove parenthesis from training data"""
+def parenthesis_correcting(input_df):
+    """Turns parenthesis into COMMENT"""
     df = input_df.copy()
-    return df.loc[df['parenthesis_flag'] != "YesPAREN",:]
+    opened = df.loc[df.text.str.match('^\($')].index
+    closed = df.loc[df.text.str.match('^\)$')].index
+    parenthesis_groups = zip(opened,closed)
+    # intialize
+    p = next(parenthesis_groups)
+    arr = []
+    for i in range(len(df)):
+        if i > p[1]:
+            p = next(parenthesis_groups, (0,0))
+        if (i >= p[0]) & (i <= p[1]):
+            arr.append(True)
+        else:
+            arr.append(False)
+    # make parenthesis comments
+    # if removing parenthsis, switch True and False then filter out.
+    df.loc[arr,"label"] = "COMMENT"
+    return df
 
-def ingredient_has_name(d):
+def ingredient_has_name(df):
     """
     Ensure the ingredient has a name within the text
     """
-    return any(d.label.str.match('^NAME$'))
+    return any(df.label.str.match('^NAME$'))
 
-def find_hyphen_ingredients(d):
-    """Find units and qtys with hyphens in them.
+def remove_hyphen_ingredients(df):
+    """Find and rmeove units and qtys with hyphens in them.
     These instances happen in examples like '1-pound'
     and is incorrectly labelled just a qty or just a unit.
-
-    Returns False if this occurs in the ingredient
     """
+    d = df.copy()
     hyphen_in = d.text.str.contains("-")
     not_hyphen_only = ~d.text.str.match('^-$')
     qty = d.label.str.match('^QTY$') 
     unit = d.label.str.match('^UNIT$')
     qty_or_unit = (qty | unit)
-    return any(not_hyphen_only & (hyphen_in & qty_or_unit))
+    filter_ids = d.loc[(not_hyphen_only & (hyphen_in & qty_or_unit))].ID
+    return d[~d['ID'].isin(filter_ids)].reset_index(drop=True)
 
 def or_to_comment(df):
     """
@@ -86,42 +101,53 @@ def or_to_comment(df):
     come after the first or and replace all text after them
     as a comment.
     """
-    # make a copy of the df
     or_ingr = df.copy()
-    or_index = or_ingr[or_ingr['text'] ==  "or"].index
-    first_name_index = or_ingr[or_ingr['label'] ==  "NAME"].index[0]
+    first_name_ingredients = or_ingr[or_ingr['label'] ==  "NAME"].drop_duplicates("ID")
+    ingredient_with_name_id = list(first_name_ingredients.ID)
+    or_ingr = or_ingr[or_ingr['ID'].isin(ingredient_with_name_id)].reset_index(drop = True) # filter out ingredients with no names in them.
+    or_index = or_ingr[or_ingr['text'] ==  "or"].index # get index of 'or'
+    first_name_index = first_name_ingredients.index # index of first names for each ingredient
+    new_id_index = or_ingr.drop_duplicates("ID").index  # indexes for when new ingredient starts
 
-    or_replacement_index = or_index[or_index > first_name_index]
-    if or_replacement_index.values.size > 0:
-        or_ingr.loc[or_replacement_index[0]:, "label"] = "COMMENT"
+    iter_id = iter(new_id_index)
+    iter_name = iter(first_name_index)
+    iter_or = iter(or_index)
+    id_ = next(iter_id) # intialize the ID
+    or_ = next(iter_or) # initialize the or index
+
+    arr = []
+    for i in range(len(or_ingr)):
+        if i == id_: # new ingredient, reset val
+            val = False
+            id_ = next(iter_id, None) # get next ingredient index
+            name_ = next(iter_name, None) # initialize the name
+
+        if val: # if we are commenting out, continue to comment out
+            if i == or_:
+                or_ = next(iter_or, None) # if more 'or's appear in alternative ingredients, skip them.
+            arr.append(True)
+            continue
+
+        if i == or_: # if the text is or, evaluate
+
+            if name_ < or_: # if name came before or, comment out the or's
+                val = True # initialize the comment out markers
+            else:
+                val = False
+            or_ = next(iter_or, None)
+
+        arr.append(val)
+
+    or_ingr.loc[arr, "label"] = "COMMENT"
     return or_ingr
 
-def run_data_cleaning(input_df):
-	"""
-	Cleanses training data of text that increases complexity
-	or that will cause issues while creating the model.
-	"""
-	print("Cleaning phrase tagger data...")
-	df = remove_parenthesis(input_df)
-	df.loc[:, 'label'] = df.loc[:, 'label'].apply(remove_label_prefix)
-	df.loc[:, 'label'] = df.loc[:, 'label'].apply(replace_INDEX_with_QTY)
-	df.loc[:, 'text']  = df.loc[:, 'text'].apply(remove_QTY_symbol)
-
-	df_len = len(df.ID.unique())
-	cleaned_df = []
-	for d in iter(df.groupby("ID")):
-		print(f"{round((int(d[0])/df_len)*100,3)}%", end='\r')
-		# Select the df and the columns we need
-		ingredient_df = d[1].loc[:, ('text', 'label', 'ID')]
-		# If no hyphen errors and ingredient has a NAME
-		if ((not find_hyphen_ingredients(ingredient_df))
-				& (ingredient_has_name(ingredient_df))):
-			# Handle when there is an or in the ingredient
-			if any(ingredient_df.text.str.match('^or$')):
-				ingredient_df = or_to_comment(ingredient_df)
-			cleaned_df.append(ingredient_df)
-
-	return pd.concat(cleaned_df)
+def run_data_cleaning(df, *funcs):
+    """
+    Cleanses training data of text that increases complexity
+    or that will cause issues while creating the model.
+    """
+    print("Cleaning phrase tagger data...")
+    return reduce(lambda arg, func: func(arg), funcs, df)
 
 print("Reading in ingredient_phrase_tagger_training_data...")
 df = pd.read_csv(in_file_path, sep='\t', header=None)
@@ -134,6 +160,14 @@ print(df.head(5))
 print(f"Writing formatted dataset to {out_file_path}")
 df.to_csv(out_file_path, index=False)
 
-cleaned_df = run_data_cleaning(df)
+cleaned_df = run_data_cleaning(
+    df, 
+    remove_label_prefix,
+    replace_index_with_qty,
+    remove_qty_symbol,
+    parenthesis_correcting,
+    remove_hyphen_ingredients,
+    or_to_comment,
+)
 print(f"Writing cleaned dataset to {clean_data_out_file_path}")
 cleaned_df.to_csv(clean_data_out_file_path, index=False)
